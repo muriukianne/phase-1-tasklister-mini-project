3a0b652517d17d5f1528fa39d0c9b38212f0f71f
Anne Muriuki <muriukianne05@gmail.com>
2024-10-06 15:44:56 +0300

    using DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const createtask = document.getElementById ("create-task-form")
+  createtask.addEventListener ('submit', (event) => {
+    event.preventDefault();
+
+     const taskdescription = document.getElementById("new-task-description").value ;
+     const duedate = document.getElementById("due_date").value ;
+     const priority = document.getElementById("priority").value ;
+
+     console.log( taskdescription , duedate , priority )
+     
+     tasks.innerHTML += `<h5>* ${taskdescription}</h5> <h5>Duedate: ${duedate} </h5> <h5>Priority: ${priority} </h5> `;
+
+     })
+
+     })
+     
+
+    
