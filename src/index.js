757bcff8babd590a3f49f356ce7fcb5b012c4683
Anne Muriuki <muriukianne05@gmail.com>
2024-10-06 20:40:55 +0300

    add DOM

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const createtask = document.getElementById ("create-task-form")
-  createtask.addEventListener ('submit', (event) => {
-    event.preventDefault();
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let taskdescription = document.getElementById('new-task-description').value
+    let duedate = document.getElementById("due_date").value
+    let priority = document.getElementById("priority").value
 
-     const taskdescription = document.getElementById("new-task-description").value ;
-     const duedate = document.getElementById("due_date").value ;
-     const priority = document.getElementById("priority").value ;
+    buildToDo(taskdescription,duedate,priority)
+    form.reset()
+  })
 
-     console.log( taskdescription , duedate , priority )
-     
-     tasks.innerHTML += `<h5>* ${taskdescription}</h5> <h5>Duedate: ${duedate} </h5> <h5>Priority: ${priority} </h5> `;
+});
 
-     })
+function buildToDo(taskdescription,duedate,priority) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  
+  btn.textContent = 'x'
+  li.textContent += ` *${taskdescription}   Duedate:${duedate}   Priority:${priority} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
 
-     })
-     
-
-    
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
